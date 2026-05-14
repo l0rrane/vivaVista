5b2e59bf87a169becba10ed7ed61560c39f2541c
l0rrane <gabrielalorraneespin@gmail.com>
2026-05-14 13:51:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+    }
+
+  })
+
+})
+
+document.querySelectorAll('.fade').forEach((el) => {
+  observer.observe(el)
+})
